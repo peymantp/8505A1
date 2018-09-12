c6d59855f96560430a5bb9d7ae39634258acac09
Peyman Tehrani Parsa <peymantparsa@gmail.com>
2018-09-12 03:13:02 -0700

    working covert channel

--- sneaky.py
+++ sneaky.py
@@ -8,7 +8,8 @@ from scapy.all import *
 
 '''
 Program argument initialization 
-./client.py -s 192.168.0.4 -sport 80 -d 192.168.0.7 -dport 80 -f ./server.py -t TCP -mode server
+./sneaky.py -s 192.168.0.4 -sport 80 -d 192.168.0.7 -dport 80 -f ./sneaky.py -t TCP -mode server
+sudo python3 sneaky.py -f ./server.py -t TCP -mode server
 '''
 arg_parser = argparse.ArgumentParser(
     prog='Covert Channel',
@@ -16,7 +17,7 @@ arg_parser = argparse.ArgumentParser(
 )
 arg_parser.add_argument('-s',dest='src_ip', help='IP of computer being bounced off of')
 arg_parser.add_argument('-sport',dest='src_port', help='Port of computer being bounced off of', const=80, nargs='?')
-arg_parser.add_argument('-d',dest='des_ip', help='IP of server',required=True)
+arg_parser.add_argument('-d',dest='des_ip', help='IP of server')
 arg_parser.add_argument('-dport',dest='des_port', help='Port of server', const=80, nargs='?')
 arg_parser.add_argument('-f','-file',dest='file', help='File being sent')
 arg_parser.add_argument('-msg',dest='msg', help='Text being sent')
@@ -24,19 +25,21 @@ arg_parser.add_argument('-t','-transport',dest='transport', help='TCP or UDP')
 arg_parser.add_argument('-mode',dest='mode', help='server or client')
 args = arg_parser.parse_args()
 
-que
+que_out=None
+que_in=[]
 packet_num=0
 
-if args.file is not None:
-    print('reading file')
-    with open(args.file, 'r') as f:
-        que = f.read()
-    print(que)
-elif args.msg is not None:
-    que = args.msg
-else:
-    print('Select either file or massage to send')
-    exit()
+def client_input():
+    if args.file is not None:
+        print('reading file')
+        with open(args.file, 'r') as f:
+            que_out = f.read()
+        print(que_out)
+    elif args.msg is not None:
+        que_out = args.msg
+    else:
+        print('Select either file or massage to send')
+        exit()
 
 def craft(character):
     global packet_num
@@ -52,7 +55,7 @@ def craft(character):
     return pck
     
 def client():
-    for char in que:
+    for char in que_out:
         pkt = craft(char)
         print(pkt)
         send(pkt)
@@ -61,15 +64,22 @@ def client():
 def parse_tcp(pkt: Packet):
     flags=pkt['TCP'].flags
     if flags == 0x042:
-        print(pkt['TCP'].ttl)
+        que_in.append(chr(pkt['IP'].ttl))
+        print(''.join(que_in))
         
 def parse_udp(pck):
     return pck
 
 def server():
     if args.transport == 'TCP':
+        print('Detecting TCP packets')
         sniff(filter="tcp", prn=parse_tcp)
     elif args.transport == 'UDP':
+        print('Detecting UDP packets')
         sniff(filter="udp", prn=parse_udp)
 
-client()
+if args.mode == 'server':
+    server()
+elif args.mode == 'client':
+    input()
+    client_input()
